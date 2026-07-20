b7b1932195ea95f6d616f9589bccc3dab4ead4a0
JosuFacha14 <josuedavid.ortiz05@gmail.com>
2026-07-19 18:16:50 -0600

    borrar contraseña de db

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const mysqlConnection = mysql.createPool({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'Joel1234',
+  password: '',
   database: 'BDNovaEventG5',
   multipleStatements: true,
   waitForConnections: true,
